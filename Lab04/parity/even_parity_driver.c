968cdf2f987aa80c13574fb688b78734898d13c3
FPGA7777 <55390129+FPGA7777@users.noreply.github.com>
2019-10-30 22:44:53 +0800

    Update even_parity_driver.c

--- Lab04/parity/even_parity_driver.c
+++ Lab04/parity/even_parity_driver.c
@@ -2,10 +2,10 @@
 #include "parity_generator.h"
 
 /************************** Function Definitions ***************************/
-u32 parity( u32 data ){
+u32 parity( UINTPTR baseAddr, u32 data ){
 	u32 ans;
 	
-	PARITY_GENERATOR_mWriteReg(baseAddr, 1, data);
-	ans = PARITY_GENERATOR_mReadReg (baseAddr, 0);
+	PARITY_GENERATOR_mWriteReg( baseAddr, 4, data );
+	ans = PARITY_GENERATOR_mReadReg ( baseAddr, 0 );
 	return ans;
 }
