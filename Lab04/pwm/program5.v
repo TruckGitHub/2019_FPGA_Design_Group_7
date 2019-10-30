4abe3ea68900625b76e2a2bc5e30acb7db87d544
FPGA7777 <55390129+FPGA7777@users.noreply.github.com>
2019-10-30 22:33:45 +0800

    Update program5.v

--- Lab04/pwm/program5.v
+++ Lab04/pwm/program5.v
@@ -1,7 +1,7 @@
 module pwm( R_value, G_value, B_value, data );
 
 input [31:0] data;
-output [7:0] R_value, G_value, B_value;
+output  wire [7:0] R_value, G_value, B_value;
 
 assign R_value = data [23:16];
 assign G_value = data [15:8];
