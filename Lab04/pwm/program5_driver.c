62f8cef6b40ea73bdcec2e3ce90dfe67c232104c
FPGA7777 <55390129+FPGA7777@users.noreply.github.com>
2019-10-30 22:32:20 +0800

    Create program5_driver.c

---
+++ Lab04/pwm/program5_driver.c
@@ -0,0 +1,8 @@
+/***************************** Include Files *******************************/
+#include "pwm.h"
+
+/************************** Function Definitions ***************************/
+void pwm_led( UINTPTR baseAddr, u32 R, u32 G, u32 B ){
+	u32 data = ( R << 16 ) + ( G << 8 ) + B;
+	PWM_mWriteReg(baseAddr, 0, data);
+}
