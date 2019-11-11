6441fe89e2e0b5bed8caea0ed177e6903ec8e543
Y.Y. Huang <53553438+yuyun-haha@users.noreply.github.com>
2019-11-11 16:40:52 +0800

    ~

--- Lab04/hw4/hw4.srcs/djb2_driver.cpp
+++
@@ -1,8 +0,0 @@
-u32 djb2(UINTPTR baseAddr, u32 data_in {
-	u32 hash_value;
-	u32 data_in;
-	ADDER_mWriteReg(baseAddr, 0, data_in);
-	hash_value = ADDER_mReadReg (baseAddr, 4);
-	return hash_value;
-
-}
